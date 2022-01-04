2941b910b31c3c2e36b956d85648e2eb87d5181d
daplaci <davide.placido@cpr.ku.dk>
2022-01-04 22:25:34 +0100

    add preliminary rook movement

--- pieces.js
+++ pieces.js
@@ -3,7 +3,9 @@ class Piece {
         this.name = name;
         this.color = color;
         this.position = position;
+        this.default_position = position;
         this.display = true;
+        this.is_eaten = false;
         this.img = loadImage('imgs/'+this.name + '_' + this.color + '.png');
         if (this.constructor == Piece) {
           throw new Error("Abstract classes can't be instantiated.");
@@ -11,6 +13,9 @@ class Piece {
       }
     move(){
         throw new Error("Method 'move()' must be implemented.");
+    }
+    eat(){
+        throw new Error("Method 'eat()' must be implemented.");
     }
     
     show_piece_at_position(){
@@ -25,7 +30,26 @@ class Pawn extends Piece {
     }
     move(position){
         //here is the only place where the piece is moved
-        this.position = position
+        if (this.position == this.default_position){
+            if (this.color == 'black'){
+                if (position == this.position+16){
+                    this.position = position
+                }
+            }else{
+                if (position == this.position - 16){
+                    this.position = position
+                }
+            }
+        }
+        if (this.color == 'black'){
+            if (position >= (this.position+7) && position <= (this.position+9)){
+                this.position = position
+            }
+        }else{
+            if (position >= (this.position-9) && position <= (this.position-7)){
+                this.position = position
+            }
+        } 
     }
 }
 
@@ -35,8 +59,18 @@ class Rook extends Piece {
     }
     move(position){
         //here is the only place where the piece is moved
+        if ((position - this.position)%8 == 0)  {
+            this.position = position
+        }
+        let start = min(position, this.position)
+        let end = max(position, this.position)
+        for (var i = start+1; i <end+1; i++){
+            if (i%8 ==0){
+                return
+            }
+        }
         this.position = position
-    }
+        }
 }
 
 class Knight extends Piece {
